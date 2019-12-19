ed3163f3c375d2a250317f03f9059a8f98738b08
Facundo Fierro <ffierro@navent.com>
2019-12-18 21:02:38 -0300

    Ejercicio inicial Golang

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//cards := newDeck()
+
+	//cards.saveToFile("/home/ffierro/Documents/my-cards.txt")
+
+	// newDeckFromFile("my-").print()
+	cards := newDeck()
+
+	cards.print()
+
+}
